100c3dc54c9370921f9a56c828a2789ea28e3e97
fabio <fabio_batoni@hotmail.com>
2024-01-14 13:49:58 -0300

    create new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule, RouterOutlet, NewComponent],
+  template: `
+      <h1>Curso de Angular</h1>
+      <app-new-component />
+    `
 })
 export class AppComponent {
   title = 'first-steps-angular';
 }
+
